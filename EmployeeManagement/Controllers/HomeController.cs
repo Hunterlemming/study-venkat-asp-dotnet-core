9c0f2ebf4461495f5c4ea8979309fd5015082893
Hunterlemming <kklemming@gmail.com>
2020-11-21 13:14:18 +0100

    Setup mvc in asp net core

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello from MVC";
+        }
+    }
+}
--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -26,6 +26,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,15 +38,8 @@ namespace EmployeeManagement
             }
 
             app.UseStaticFiles();
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hosting Environment: " + env.EnvironmentName);
-                });
-            });
+            app.UseMvcWithDefaultRoute();
+            
         }
     }
 }
