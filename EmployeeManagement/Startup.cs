7ce3910cdcf2d9c5636c181d5cd3558940b44831
Hunterlemming <kklemming@gmail.com>
2020-11-21 19:26:24 +0100

    32. Routing in ASP NET Core MVC

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -40,7 +40,11 @@ namespace EmployeeManagement
             }
 
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
+            
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
             
         }
     }
