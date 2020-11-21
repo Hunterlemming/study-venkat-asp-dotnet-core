868cd9919079cfaf3d772e289d2c6bf97840da37
Hunterlemming <kklemming@gmail.com>
2020-11-21 12:57:43 +0100

    13. ASP NET Core developer exception page

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -33,20 +33,22 @@ namespace EmployeeManagement
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions
+                {
+                    SourceCodeLineCount = 10
+                };
+                app.UseDeveloperExceptionPage(developerExceptionPageOptions);
             }
 
-            FileServerOptions fileServerOptions = new FileServerOptions();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
-            app.UseFileServer(fileServerOptions);
+            // app.UseFileServer();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    throw new Exception("Some error processing the request");
+                    // await context.Response.WriteAsync("Hello World!");
                 });
             });
         }
